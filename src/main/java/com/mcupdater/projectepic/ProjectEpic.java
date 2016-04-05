318a4904c106bc8cde268bb3eae7e567dbc251fc
Scott M. Barbour <smbarbour@gmail.com>
2016-04-04 22:04:21 -0500

    First code

---
+++ src/main/java/com/mcupdater/projectepic/ProjectEpic.java
@@ -0,0 +1,14 @@
+package com.mcupdater.projectepic;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = "projectepic", useMetadata = true)
+public class ProjectEpic {
+
+	@EventHandler
+	public void init(FMLInitializationEvent ev) {
+
+	}
+}
